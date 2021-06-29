5e3a48dc2cd8ab0ae8fad1e031086bd557c809f1
Mudit Gurjar <mudit182@gmail.com>
2021-06-29 09:05:22 +0530

    - python functions in folder - added commands.txt files

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-#import tensorflow
-#from tensorflow import keras
-import function1
-import function2
+# #import tensorflow
+# #from tensorflow import keras
+# import fullName
+# from age import findAge
 
-year = int(input("Enter current year:"))
-yob = int(input("Enter date of birth:"))
-age = function1.find_age(year, yob)
+# year = int(input("Enter current year:"))
+# yob = int(input("Enter date of birth:"))
+# age = findAge.find_age(year, yob)
 
-name= input("Enter first name: ")
-surname= input("Enter surname: ")
-full_name= function2.get_name(name, surname)
+# name= input("Enter first name: ")
+# surname= input("Enter surname: ")
+# full_name= fullName.get_name(name, surname)
 
-print("Name:", full_name)
-print("Age: ", age)
+# print("Name:", full_name)
+# print("Age: ", age)
+
+
+import keras
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
